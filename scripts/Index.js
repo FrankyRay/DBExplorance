d89be2031dbd13b170da18d2af5b9e3a6d1a3d53
FrankyRay <frankyraymarcellsinaga@gmail.com>
2022-10-02 21:28:22 +0700

    Revamping Custom Commands (WIP) and Adding In-Game Setting

--- scripts/Index.js
+++ scripts/Index.js
@@ -1,6 +1,6 @@
 import "./api/CustomCommand/IndexCCommand";
 import "./api/ConsoleCommandUI/IndexConsoleC";
-import "./api/Events/MainEvent";
+import "./api/GametestOptions/Events";
 import "./api/Watchdog";
 import "./api/InfoTick/MainTick";
 import "./api/ChangePermutation";
